2328ffe2b65f47ec39dcc54443b27d304ff5b611
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:11:17 -0500

    v1

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,14 @@
-﻿Public Class Form1
+﻿Option Strict On
+Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
 
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Dim Number As Integer = 1
 
+        Do While Number <= 10
+            lstResult.Items.Add("The perfect square is: " & Number ^ 2)
+            Number = Number + 1
+        Loop
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -1,10 +1,16 @@
 ﻿Option Strict On
-
 Public Class Form1
-
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim Population As Double = 7000
+        Dim Year As Integer = 2012
+
+        Do While Population > 6
+            Year = Year - 50
+            Population = Population / 2
+        Loop
+
+        MessageBox.Show("The year is: " & Year)
 
     End Sub
 End Class
